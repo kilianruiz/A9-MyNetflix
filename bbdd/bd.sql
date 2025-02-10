045347dc259fd406a5b00dcfcffd06e826c6a14c
kilianruiz <451093.joan23@fje.edu>
2025-02-10 16:37:39 +0100

    Cambio en la BBDD

--- bbdd/bd.sql
+++ bbdd/bd.sql
@@ -6,12 +6,12 @@ CREATE TABLE peliculas (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255),
   poster VARCHAR(255),
-  description TEXT,
+  descripcion TEXT,
   likes INT
 );
 
 -- Insertar datos en la tabla de películas
-INSERT INTO peliculas (title, poster, description, likes) VALUES
+INSERT INTO peliculas (title, poster, descripcion, likes) VALUES
 ('Breaking Bad', './img/bb.webp', 'Serie de drama sobre un profesor de química que se convierte en fabricante de metanfetaminas.', 250),
 ('La Casa de Papel', './img/lcdp.webp', 'Un grupo de delincuentes lleva a cabo un asalto a la Real Casa de la Moneda de España.', 150),
 ('Fast & Furious 8', './img/ff8.webp', 'La octava entrega de la saga Fast & Furious, centrada en traiciones y carreras de autos.', 300),
